9d9be0c4ebf8f8ae79725cdf82686eb2be685fd2
Suman Bhattacharjee <80640390+zoforous@users.noreply.github.com>
2025-06-23 17:31:57 +0530

    Delete virtual-learning-environment directory

--- virtual-learning-environment/includes/auth_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if (!isset($_SESSION['user'])) {
-    header("Location: /login.php");
-    exit();
-}
-?>
--- virtual-learning-environment/includes/footer.php
+++
@@ -1,6 +0,0 @@
-    </main>
-    <footer>
-        <p>&copy; <?php echo date("Y"); ?> Virtual Learning Environment. All rights reserved.</p>
-    </footer>
-</body>
-</html>
